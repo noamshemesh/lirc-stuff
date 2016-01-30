85f0fe913f35abf19220149503d19f0fcc1d7c0b
Noam Shemesh <noamshemesh@gmail.com>
2016-01-30 16:50:16 +0200

    add all off activity

--- activities/EverythingOff.py
+++ activities/EverythingOff.py
@@ -1,9 +1,13 @@
 from subprocess import call
+from time import sleep  
 
-def send_once(self, device_id, message):
-  call(['irsend', 'SEND_ONCE', device_id, message, '-d', '/var/run/lirc/lircd'])
+def send_once(device_id, message, count=1):
+  call(['irsend', 'SEND_ONCE', device_id, message, '-d', '/var/run/lirc/lircd', '-#', str(count)])
 
 if __name__ == "__main__":
-  send_once('Onkyo_RC-710M', 'POWEROFF')
-#  send_once('
+  send_once('Onkyo_RC-710M', 'KEY_POWEROFF')
+  sleep(0.3)
+  send_once('XBOX-ONE', 'KEY_POWEROFF', 2)
+  sleep(0.3)
+  send_once('Samsung_BN59-00685A', 'KEY_POWEROFF', 2)
 
